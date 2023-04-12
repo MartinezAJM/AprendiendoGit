d4e8bdc174f9714eda7693da951ca619ba1bf636
Vanessa Rubiera <83053924+VanessaRubiera@users.noreply.github.com>
2023-04-11 19:45:23 -0700

    Add code in main.py

---
+++ main.py
@@ -0,0 +1,18 @@
+# Proyecto para practicar ramas
+
+"""
+Comentario
+multilinea en python
+"""
+
+print("Basic Operation")
+
+"""
+Python tiene como buena práctica identar bloques de códigos -stagments- declaracion """
+
+"""
+Para ejecutar un script de python 
+
+1. Tener un archivo .py
+2. Poner el comando python archivo.py
+"""
